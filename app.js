3aeec3f27d764932e2ffe214d09377b43637264b
Vadim <_s_e_t__@ukr.net>
2025-03-16 14:49:32 +0200

    add some code

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors')
 
 const app = express();
 
+app.use(express.json());
+app.use(cors())
+
+const routerProducts = require("./routers/products");
+
+app.use("/api/products", routerProducts);
 
 module.exports = app;
---
+++ controllers/products.js
@@ -0,0 +1,5 @@
+exports.getAll = (req, res) => {
+    res.json({
+        message:'Success'
+    })
+}
