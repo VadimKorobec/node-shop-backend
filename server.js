7e745fa6c006ac43e4f57172da35753c8d5111df
Vadim <_s_e_t__@ukr.net>
2025-03-16 12:55:58 +0200

    add some code

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const DB_HOST =
-  "mongodb+srv://Vadim:<db_password>@cluster0.wllpr.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://Vadim:IJcI7iwfLNLqSsw9@cluster0.hid1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
 mongoose
-  .connect()
+  .connect(DB_HOST)
   .then(() => {
     app.listen(8000);
     console.log("Connect success");
   })
   .catch((err) => {
-    console.log(err.message);
+      console.log(err.message);
+      process.exit(1)
   });
-
-  //IJcI7iwfLNLqSsw9
